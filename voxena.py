4b3db27b0d9fd4c10e90b28be454e6b75b769cba
Nougator <37145431+nougator@users.noreply.github.com>
2020-05-03 21:57:24 +0200

    Added basics functions
    They don't do anything right now

--- voxena.py
+++ voxena.py
@@ -1,9 +1,28 @@
+from threading import Thread
 import socket
 
 
-class Voxena:
-    def start(self, port, ip):
-        if ip:
+class Voxena(Thread):
+    def __init__(self, port, ip):
+        if ip is not None:
             ip = ip
         else:
             ip = '127.0.0.1'
+
+    sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM)
+
+    def stop(self):
+        self.sock.close()
+
+    def generateChuncks(self, id):
+        pass
+
+    def getChunks(self, id):
+        pass
+
+    def updateChunks(self):
+        pass
+
+    def ping(self):
+        pass
